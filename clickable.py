9faa83c4bb8bcb30f2dcc016f21892af48ecf276
Daniel-C56-Pd3 <danield494@nycstudents.net>
2025-01-02 10:15:48 -0500

    dasd

--- clickable.py
+++ clickable.py
@@ -1,9 +1,8 @@
+import tkinter as tk
 def on_button_click():
     print("Game Starting")
 
 root = tk.Tk()
 root.title("Clickable Button Example")
-
 button = tk.Button(root, text="Play Game", command=on_button_click)
 button.pack(pady=20)
-break
