8d33fefbd1cba6638a632b18cb39dfa7125d3e15
Daniel-C56-Pd3 <danield494@nycstudents.net>
2025-01-06 09:47:50 -0500

    sad

--- clickable.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-def on_button_click():
-    print("Game Starting")
-
-root = tk.Tk()
-root.title("Clickable Button Example")
-button = tk.Button(root, text="Play Game", command=on_button_click)
-button.pack(pady=20)
